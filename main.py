3ba3a8ac40b890f53375878d784503b64326707a
Felipe Guimaraes <guima.felipec@gmail.com>
2018-10-25 22:37:51 -0200

    added gitignore

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
 from board import *
 from utility import *
 
+def move(i, j, ut, bo): # N = 0, L = 1, S = 2, O = 3
+    rf = bo.board[i][j]
+    value, direction = rf + 0.6*ut.get(i-1,j) + 0.4*ut.get(i,j-1), 0
+
+    curr_val = rf + 0.6*ut.get(i,j-1) + 0.4*ut.get(i-1,j)
+    if curr_val > value:
+        value, direction = curr_val, 1
+
+    curr_val = rf + 0.6*ut.get(i+1,j) + 0.4*ut.get(i,j-1)
+    if curr_val > value:
+        value, direction = curr_val, 2
+
+    curr_val = rf + 0.6*ut.get(i+1,j) + 0.4*ut.get(i+1,j)
+    if curr_val > value:
+        value, direction = curr_val, 3
+
+    return value, direction
+
+
+def bellman_it(ut, bo):
+    nu = Utility(4,8)
+    policy = [[0]*4]*8
+    for i in range(4):
+        for j in range(8):
+            if(bo.board[i][j].reset == False):
+                nu = move(i, j, ut, bo)
+
+
 if __name__ == "__main__":
     u = Utility(4, 8)
     b = Board(4,8)
--- utility.py
+++ utility.py
@@ -3,3 +3,14 @@ class Utility:
         self.n = n
         self.m = m
         self.board = [[0]*m]*n
+    
+    def get(self, i, j):
+        to_ret = 0
+        if (i < 0 or i >= self.n) and (j < 0 or j >= self.m):
+            to_ret -= 10
+        if i < 0: i = 0
+        if i >= self.n: i = self.n - 1
+        if j < 0: j = 0
+        if j >= self.m: j = self.m -1
+        
+        return to_ret + self.board[i][j]
